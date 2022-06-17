24069852d030a6d7c4dd3ca75d2b38c6768af9c8
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2022-06-17 23:12:02 +0300

    Added home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,14 @@
+import './Home.css';
+
 const Home = () => {
     return (
-        <h1>Home page</h1>
+        <section className="flex flex-col items-center">
+            <img src="./postbank-logo.png" alt="Postbank logo" className="absolute w-1/4 top-56 z-10" />
+            {/* <h1 className="absolute text-8xl text-center uppercase font-bold italic black top-56 z-10">Postbank</h1> */}
+            <h2 className="home-subheading absolute text-6xl text-center font-bold italic black z-10">Solutions for your tomorrow.</h2>
+
+            <div className="home bg-red-300"></div>
+        </section>
     );
 }
 
